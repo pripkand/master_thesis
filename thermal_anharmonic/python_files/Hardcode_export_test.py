52461b9e04c8345443e4d55991e98c2eb51da0fe
Peter Ripoll Kandylas <pripkand@protonmail.com>
2025-12-10 20:54:16 +0100

    Trying to trouble shoot why it doesnt work, it seems like the constraints are the issue.

--- thermal_anharmonic/python_files/Hardcode_export_test.py
+++ thermal_anharmonic/python_files/Hardcode_export_test.py
@@ -10,7 +10,7 @@ M=cp.bmat([
     [0,P2,1j/2,X3,XP2,X2P,X2P],
     [0,-1j/2,P2,X2P,P3,XP2,XP2],
     [P2,X3,X2P,X4,X2P2,X3P,-1J*P2+X3P],
-    [P2,XP2,P3,0,P4,-1J*2*P2+XP3,-1J*3*P2+XP3],
+    [P2,XP2,P3,X2P2,P4,-1J*2*P2+XP3,-1J*3*P2+XP3],
     [-1J/2,X2P,XP2,-1j*3*P2+X3P,-1J*P2+XP3,1+X2P2,1/2+X2P2],
     [1J/2,X2P,XP2,-1j*2*P2+X3P,XP3,1/2+X2P2,1*X2P2]
 ])
@@ -22,7 +22,7 @@ beta=cp.Parameter(nonneg=True)
 constant=cp.Constant(np.array([[1]]))
 
 constraints=[M>>0,cp.bmat([[constant,z_matrices[1]],[z_matrices[1],constant]])>>0,cp.bmat([[z_matrices[1],z_matrices[2]],[z_matrices[2],constant]])>>0,cp.bmat([[z_matrices[2],z_matrices[3]],[z_matrices[3],constant]])>>0,
-            cp.bmat([[z_matrices[-1]-constant-t_matrices[0],-np.sqrt(0)*t_matrices[0]],[cp.Constant([[0]]),constant]])>>0,cp.bmat([[z_matrices[-1]-constant-t_matrices[1],-np.sqrt(0.355051)*t_matrices[1]],[-np.sqrt(0.355051)*t_matrices[1],constant-0.355051*t_matrices[1]]])>>0,
+            cp.bmat([[z_matrices[-1]-constant-t_matrices[0],-np.sqrt(0)*t_matrices[0]],[cp.Constant(np.array([[0]])),constant]])>>0,cp.bmat([[z_matrices[-1]-constant-t_matrices[1],-np.sqrt(0.355051)*t_matrices[1]],[-np.sqrt(0.355051)*t_matrices[1],constant-0.355051*t_matrices[1]]])>>0,
             cp.bmat([[z_matrices[-1] - constant - t_matrices[2], -np.sqrt(0.844949) * t_matrices[2]],
                       [-np.sqrt(0.844949) * t_matrices[2], constant - 0.844949* t_matrices[2]]]) >> 0,
             1/9*t_matrices[0]+0.512486*t_matrices[1]+0.376403*t_matrices[2]==0
@@ -31,7 +31,7 @@ constraints=[M>>0,cp.bmat([[constant,z_matrices[1]],[z_matrices[1],constant]])>>
 objective=cp.Minimize(2*P2)
 problem=cp.Problem(objective, constraints)
 
-problem.solve(solver='SDPA',verbose=True)
+problem.solve(verbose=True)
 
 print(problem.status)
 print(problem.value)
--- thermal_anharmonic/python_files/thermal_harmonic_oscilaltor_test.py
+++ thermal_anharmonic/python_files/thermal_harmonic_oscilaltor_test.py
@@ -4,13 +4,14 @@ import h5py
 import json
 
 from cvxpy import length
+from cvxpy.reductions.solvers.conic_solvers import CLARABEL
 
 from header_file import build_matrix,run_sdp
 
-in_file = "/home/pripoll/Documents/Uni_Classes/Masters_thesis/anharmonic_thermal/thermal_anharmonic/wolfram_output/Harmonic_output_for_l=4_m=3_k=3.json"
+in_file = "/home/pripoll/Documents/Uni_Classes/Masters_thesis/anharmonic_thermal/thermal_anharmonic/wolfram_output/harmonic_output_for_l=4_m=3_k=3.json"
 out_folder = "python_outputs"
 
-temperature_range=np.arange(0.02,0.6,0.02)
-run_sdp(np.array([1/T for T in temperature_range]),in_file,out_folder)
+beta_range=np.arange(.7,1,.1)
+run_sdp(beta_range,in_file,out_folder)
 
 
