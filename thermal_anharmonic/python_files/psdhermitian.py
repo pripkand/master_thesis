636d6f4b615fc6f7eebbddd58f01f240b4de758f
Peter Ripoll Kandylas <pripkand@protonmail.com>
2026-01-14 22:32:07 +0100

    I believe the class is mostly done. The only thing left to do is: Add constructors for specific class initialisations (T and Z matrices) and add a method that conjoins 4 class instances into one (basically np.block but for the class. Although I think block works fine for this). Next is implementing the reading into SDPA prob with a problem class.

--- thermal_anharmonic/python_files/psdhermitian.py
+++ thermal_anharmonic/python_files/psdhermitian.py
@@ -1,15 +1,17 @@
+from typing import MutableMapping
+
 import numpy as np
 from scipy.linalg import block_diag
 from collections import defaultdict
 
-class PsdHermitian:
+class PsdHermitian(MutableMapping):
 
     # Dunders
     def __init__(self, variables_dictionary, domain_chart, name : str, nblock : int = 1, blockstruct=None):
         self._name = name
         self._original_chart = domain_chart
-        self._matrix, self._variables = PsdHermitian.clean_up_complexes(variables_dictionary, domain_chart)
-        self._size = self._matrix[-1].shape[0]
+        self._matrix, self._variables = PsdHermitian.real_variables(variables_dictionary, domain_chart)
+        self._size = next(iter(self._matrix.values())).shape[0]
         self._nblock = nblock
         self._blockstruct = [self._size] if blockstruct is None else blockstruct
         self._mdim = len(self._variables)-1 # Number of variables is the len(variable) minus the "constant" "variable"
@@ -18,7 +20,7 @@ class PsdHermitian:
 
 
     def __str__(self):
-        return str(self._value)+"="+self._name
+        return f"Hermitian Matrix {self._name} with dimension: {self._size} blocks {self._nblock} block structure {self._blockstruct} and number of variables {self._mdim}"
 
     def __repr__(self):
         return self.__str__()
@@ -35,7 +37,7 @@ class PsdHermitian:
                 for k, v in d.items():
                     out[k] += v
 
-            return PsdHermitian(dict(out),{key:False for key in out.keys()},name=self._name+"+"+other._name,nblock=self._nblock,blockstruct=self._blockstruct)
+            return PsdHermitian(dict(out),{key:False for key in out.keys()},name=self._name+" + "+other._name,nblock=self._nblock,blockstruct=self._blockstruct)
 
     def __sub__(self, other):
         if not isinstance(other, PsdHermitian):
@@ -45,29 +47,60 @@ class PsdHermitian:
         else:
             out = defaultdict(lambda: 0)
 
-            for d in (other._matrix, self._matrix):
-                for k, v in d.items():
-                    out[k] -= v
+            for k, v in self._matrix.items():
+                out[k] += v
+            for k, v in other._matrix.items():
+                out[k] -= v
 
-            return PsdHermitian(dict(out),{key:False for key in out.keys()},name=self._name+"-"+other._name,nblock=self._nblock,blockstruct=self._blockstruct)
+            return PsdHermitian(dict(out),{key:False for key in out.keys()},name=self._name+" - "+other._name,nblock=self._nblock,blockstruct=self._blockstruct)
 
     def __neg__(self):
-        return PsdHermitian({key:-value for key,value in self._matrix},{key:False for key in self._variables},name= "-"+self._name)
+        return PsdHermitian({key:-value for key,value in self._matrix},{key:False for key in self._variables},name= "-"+self._name,nblock= self._nblock,blockstruct= self._blockstruct)
 
     def __mul__(self, other):
         if not np.isscalar(other):
             return NotImplemented
         else:
             return PsdHermitian({key: other*value for key, value in self._matrix.items()},
-                                {key: False for key in self._variables}, name=f"{other}*" + self._name)
+                                {key: False for key in self._variables}, name=f"{other}*" + self._name,nblock= self._nblock, blockstruct= self._blockstruct)
 
     def __rmul__(self, other):
         return self.__mul__(other)
 
+    def __getitem__(self, item):
+        if isinstance(item,str):
+            return self._matrix[item]
+        elif isinstance(item,int):
+            return self._matrix[self._variables[item]]
+        else:
+            return NotImplemented
+
+    def __setitem__(self, key, value):
+        if not isinstance(key, str):
+            raise TypeError("Key must be a string")
+
+        self._matrix[key] = value
+        if key not in self._variables:
+            self._variables.append(key)
+            self._mdim += 1
+
+    def __delitem__(self, key):
+        del self._matrix[key]
+        self._variables.remove(key)
+
+    def __len__(self):
+        return len(self._matrix)
+
+    def __iter__(self):
+        return iter(self._matrix)
+
+    def __contains__(self, key):
+        return key in self._matrix.keys()
+
     # Properties
     @property
     def imag(self):
-        return {key:value.imaginary for key,value in self._matrix.items()}
+        return {key:value.imag for key,value in self._matrix.items()}
     @property
     def real(self):
         return {key:value.real for key,value in self._matrix.items()}
@@ -75,17 +108,18 @@ class PsdHermitian:
     def name(self):
         return self._name
     @property
-    def shape(self):
-        return self._size,self._size
-    @property
     def size(self):
-        return self._size
+        return next(iter(self._matrix.values())).shape[0]
+    @property
+    def shape(self):
+        mat = next(iter(self._matrix.values()))
+        return tuple(list(mat.shape)[1:])
     @property
     def variables(self):
         return self._variables
     @property
     def matrix(self):
-        return self.matrix
+        return self._matrix
     @property
     def psd_form(self):
         if self._nblock == 1:
@@ -108,38 +142,55 @@ class PsdHermitian:
     # Statics
     @staticmethod
     def real_variables(var_dic,dom_chart):
-        out_dict = defaultdict(lambda:np.zeros(next(iter(var_dic)).shape),{key:np.array(value) for key,value in var_dic.items() if not dom_chart[key]})
-        variable_names = []
+        out_dict = defaultdict(lambda:np.zeros(next(iter(var_dic.values())).shape),{key:np.array(value) for key,value in var_dic.items() if not dom_chart[key]})
 
-        for key,value in set(var_dic.items())-set("constant"):
-
-            i = value.imaginary
-            r = value.real
-
+        for key, value in var_dic.items():
             if dom_chart[key]:
-
-                out_dict[key+"_real"] = np.array(r+1j*i,dtype=complex)
-                out_dict[key+"_imaginary"] = np.array(-i+1j*r,dtype=complex)
+                i = value.imag
+                r = value.real
+                out_dict[key + "_real"] = r + 1j * i
+                out_dict[key + "_imaginary"] = -i + 1j * r
 
         return out_dict, list(out_dict.keys())
 
 
     # Methods
+    def keys(self):
+        return self._matrix.keys()
+
+    def values(self):
+        return self._matrix.values()
+
+    def items(self):
+        return self._matrix.items()
+
     def rename(self,new_name):
         self._name = new_name
 
-    def direct_sum(self,other):
-        if not isinstance(other,PsdHermitian):
+    def direct_sum(self, other):
+        if not isinstance(other, PsdHermitian):
             return NotImplemented
-        else:
-            out = defaultdict(lambda:0)
-            for d in (self._matrix,other._matrix):
-                for key in d.keys():
-                    out[key]=block_diag(self._matrix[key],other._matrix[key])
 
+        out = {}
+        all_keys = set(self._matrix) | set(other._matrix)
 
-            return PsdHermitian(final_dictionary, dom_chart, name=self._name + "(+)" + other._name,nblock=self.nblock+other.nblock,blockstruct=self.blockstruct+other.blockstruct)
+        a0 = next(iter(self._matrix.values()))
+        b0 = next(iter(other._matrix.values()))
 
+        for key in all_keys:
+            a = self._matrix.get(key, np.zeros_like(a0))
+            b = other._matrix.get(key, np.zeros_like(b0))
+            out[key] = block_diag(a, b)
+
+        return PsdHermitian(
+            out,
+            {key: False for key in out},
+            name=self._name + " (+) " + other._name,
+            nblock=self.nblock + other.nblock,
+            blockstruct=self.blockstruct + other.blockstruct
+        )
+    def psd(self,key):
+        return self.psd_form[key]
 
 def symmetric_basis(n:int):
     """
--- thermal_anharmonic/python_files/test.py
+++ thermal_anharmonic/python_files/test.py
@@ -1,9 +1,15 @@
 import numpy as np
-#from psdhermitian import PsdHermitian
+from psdhermitian import PsdHermitian
+import numpy as np
 
-dictionary = {"constant":np.array([[[11,0],[0,-23]],np.zeros((2,2))]),"y1":np.array([[[10,4],[4,0]],np.zeros((2,2))]),"y2":np.array([[[0,0],[0,-8]],np.zeros((2,2))]),"y3":np.array([[[0,-8],[-8,-2]],np.zeros((2,2))])}
+dictionary_x = {"constant":np.array([[[11,0],[0,-23]],np.zeros((2,2))]),"y1":np.array([[[10,4],[4,0]],np.zeros((2,2))]),"y2":np.array([[[0,0],[0,-8]],np.zeros((2,2))]),"y3":np.array([[[0,-8],[-8,-2]],np.zeros((2,2))])}
+dictionary_z = {"constant":np.array([[[11,0],[0,-23]],np.zeros((2,2))]),"y1":np.array([[[10,4],[4,0]],np.zeros((2,2))]),"y2":np.array([[[0,0],[0,-8]],np.zeros((2,2))])}
 chart = {"constant":False,"y1":False,"y2":False,"y3":False}
-#x = PsdHermitian(dictionary,chart,name = "X")
-
-for d in ({"a":2},{"b":3}):
-    print(d)
+x = PsdHermitian(dictionary_x,chart,name = "X")
+print(x.shape)
+y = x+x
+print(y.shape)
+z = PsdHermitian(dictionary_z,chart,name = "Z")
+print(z.shape)
+z_dir_x = z.direct_sum(x)
+print(z_dir_x.shape)
